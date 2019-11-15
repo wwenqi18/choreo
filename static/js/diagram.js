c6edcc8e078b2206da62c02abcc32faeeeb4a85a
Emma <wwenqi@yahoo.com>
2019-11-14 23:12:31 -0500

    add colorpicker feature

--- static/js/diagram.js
+++ static/js/diagram.js
@@ -1,44 +1,34 @@
-var myDiagram
+var myDiagram;
 
 $(document).ready(function() {
-  var GO = go.GraphObject.make;
+  const GO = go.GraphObject.make;
+  let toolTipVisible = false;
 
-  myDiagram =
-    GO(go.Diagram, "myDiagramDiv",
-      {
-        "undoManager.isEnabled": true,  // enable undo & redo
-      });
-  
-  myDiagram.grid =
-    GO(go.Panel, go.Panel.Grid,  
-      { gridCellSize: new go.Size(25, 25) },
-      GO(go.Shape, "LineH", { stroke: "lightgrey" }),
-      GO(go.Shape, "LineV", { stroke: "lightgrey" })
-    );
+  // diagram
+  myDiagram = GO(go.Diagram, "myDiagramDiv", {
+    "undoManager.isEnabled": true, // enable undo & redo
+    BackgroundSingleClicked: hideToolTip
+  });
 
-  myDiagram.animationManager.isEnabled = false
+  myDiagram.grid = GO(
+    go.Panel,
+    go.Panel.Grid,
+    { gridCellSize: new go.Size(30, 30) },
+    GO(go.Shape, "LineH", { stroke: "lightgrey" }),
+    GO(go.Shape, "LineV", { stroke: "lightgrey" })
+  );
 
-  myDiagram.toolManager.draggingTool.isGridSnapEnabled = true
-
-  myDiagram.allowHorizontalScroll = false
-  myDiagram.allowVerticalScroll = false
-  myDiagram.allowZoom = false
-
-  // when the document is modified, add a "*" to the title and enable the "Save" button
-  // myDiagram.addDiagramListener("Modified", function (e) {
-  //   var button = document.getElementById("SaveButton");
-  //   if (button) button.disabled = !myDiagram.isModified;
-  //   var idx = document.title.indexOf("*");
-  //   if (myDiagram.isModified) {
-  //     if (idx < 0) document.title += "*";
-  //   } else {
-  //     if (idx >= 0) document.title = document.title.substr(0, idx);
-  //   }
-  // });
+  myDiagram.animationManager.isEnabled = false;
+  myDiagram.toolManager.draggingTool.isGridSnapEnabled = true;
+  myDiagram.allowHorizontalScroll = false;
+  myDiagram.allowVerticalScroll = false;
+  myDiagram.allowZoom = false;
 
   function nodeStyle() {
     return [
-      new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+      new go.Binding("location", "loc", go.Point.parse).makeTwoWay(
+        go.Point.stringify
+      ),
       {
         locationSpot: go.Spot.Center
       }
@@ -49,82 +39,155 @@ $(document).ready(function() {
     return {
       font: "11pt Helvetica, Arial, sans-serif",
       stroke: "black"
-    }
+    };
   }
 
-  myDiagram.nodeTemplateMap.add("",  // the default category
-    GO(go.Node, "Table", nodeStyle(),
-        GO(go.Shape, "Circle",
-          { row: 0, column: 0, fill: "#6c757d", strokeWidth: 0, width: 50, cursor: 'pointer' }
-        ),
-        GO(go.TextBlock, textStyle(),
-          {
-            row: 1, column: 0,
-            maxSize: new go.Size(200, NaN),
-            editable: true
-          },
-          new go.Binding("text").makeTwoWay())
-    ));
-  
-  var myPalette = GO(go.Palette, "myPaletteDiv")
-
-  myPalette.nodeTemplateMap.add("",  // the default category
-    GO(go.Node, "Table", nodeStyle(),
+  myDiagram.nodeTemplateMap.add(
+    "", // the default category
+    GO(
+      go.Node,
+      "Table",
+      nodeStyle(),
       {
-        click: mouseclick
+        click: (e, obj) => changeColor(e, obj)
       },
-      GO(go.Shape, "Circle",
-        { row: 0, column: 0, fill: "#6c757d", strokeWidth: 0, width: 50, cursor: 'pointer' }
-      ),
-      GO(go.TextBlock, textStyle(),
+      GO(
+        go.Shape,
+        "Circle",
         {
-          row: 1, column: 0,
+          row: 0,
+          column: 0,
+          fill: "#6c757d",
+          strokeWidth: 0,
+          width: 30,
+          height: 30,
+          cursor: "pointer"
+        },
+        new go.Binding("fill", "color")
+      ),
+      GO(
+        go.TextBlock,
+        textStyle(),
+        {
+          row: 1,
+          column: 0,
           maxSize: new go.Size(200, NaN),
           editable: true
         },
-        new go.Binding("text").makeTwoWay())
-    ));
+        new go.Binding("text").makeTwoWay()
+      )
+    )
+  );
 
-  myPalette.model.nodeDataArray = [
-    { text: "Dancer" }
-  ]
+  function changeColor(e, obj) {
+    if (toolTipVisible) {
+      hideToolTip();
+      return;
+    }
+
+    showToolTip(obj);
+
+    const colorpicker = document.getElementById("html5colorpicker");
+    colorpicker.onchange = function() {
+      const color = this.value;
+      const node = myDiagram.findNodeForKey(obj.part.data.key);
+      myDiagram.model.commit(m => {
+        m.set(node.data, "color", color);
+      }, "change color");
+    };
+  }
+
+  function showToolTip() {
+    const toolTipDIV = document.getElementById("toolTipDIV");
+    const pt = myDiagram.lastInput.viewPoint;
+    toolTipDIV.style.left = pt.x + 10 + "px";
+    toolTipDIV.style.top = pt.y + 10 + "px";
+    toolTipDIV.style.display = "block";
+    toolTipVisible = !toolTipVisible;
+  }
+
+  function hideToolTip() {
+    var toolTipDIV = document.getElementById("toolTipDIV");
+    toolTipDIV.style.display = "none";
+    toolTipVisible = !toolTipVisible;
+  }
+
+  // palette
+  var myPalette = GO(go.Palette, "myPaletteDiv");
+
+  myPalette.nodeTemplateMap.add(
+    "", // the default category
+    GO(
+      go.Node,
+      "Table",
+      nodeStyle(),
+      {
+        click: mouseclick
+      },
+      GO(go.Shape, "Circle", {
+        row: 0,
+        column: 0,
+        fill: "#6c757d",
+        strokeWidth: 0,
+        width: 30,
+        height: 30,
+        cursor: "pointer"
+      }),
+      GO(
+        go.TextBlock,
+        textStyle(),
+        {
+          row: 1,
+          column: 0,
+          maxSize: new go.Size(200, NaN),
+          editable: true
+        },
+        new go.Binding("text").makeTwoWay()
+      )
+    )
+  );
+
+  myPalette.model.nodeDataArray = [{ text: "Dancer" }];
 
   function mouseclick(e, obj) {
-    myDiagram.model.addNodeData({ text: "Dancer" })
+    myDiagram.model.addNodeData({ text: "Dancer" });
   }
-})
+});
 
 // Show the diagram's model in JSON format that the user may edit
-    function save() {
-      document.getElementById("mySavedModel").value = myDiagram.model.toJson();
-      myDiagram.isModified = false;
-    }
-    function load() {
-      myDiagram.model = go.Model.fromJson(document.getElementById("mySavedModel").value);
-    }
+function save() {
+  document.getElementById("mySavedModel").value = myDiagram.model.toJson();
+  myDiagram.isModified = false;
+}
 
-    // print the diagram by opening a new window holding SVG images of the diagram contents for each page
-    function printDiagram() {
-      var svgWindow = window.open();
-      if (!svgWindow) return;  // failure to open a new Window
-      var printSize = new go.Size(680, 500);
-      var bnds = myDiagram.documentBounds;
-      console.log(bnds)
-      var x = bnds.x;
-      var y = bnds.y;
-      while (y < bnds.bottom) {
-        while (x < bnds.right) {
-          var svg = myDiagram.makeSVG({ 
-            scale: 1.0, 
-            // position: new go.Point(0, 0), 
-            // size: printSize,
-            // background: "rgba(0, 255, 0, 0.5)" 
-          });
-          svgWindow.document.body.appendChild(svg);
-          x += printSize.width;
-        }
-        x = bnds.x;
-        y += printSize.height;
-      }
-      // setTimeout(function() { svgWindow.print(); }, 1);
+function load() {
+  myDiagram.model = go.Model.fromJson(
+    document.getElementById("mySavedModel").value
+  );
+}
+
+// print the diagram by opening a new window holding SVG images of the diagram contents for each page
+function printDiagram() {
+  var svgWindow = window.open();
+  if (!svgWindow) return; // failure to open a new Window
+  var printSize = new go.Size(680, 500);
+  var bnds = myDiagram.documentBounds;
+  console.log(bnds);
+  var x = bnds.x;
+  var y = bnds.y;
+  while (y < bnds.bottom) {
+    while (x < bnds.right) {
+      var svg = myDiagram.makeSVG({
+        scale: 1.0
+        // position: new go.Point(0, 0),
+        // size: printSize,
+        // background: "rgba(0, 255, 0, 0.5)"
+      });
+      svgWindow.document.body.appendChild(svg);
+      x += printSize.width;
     }
+    x = bnds.x;
+    y += printSize.height;
+  }
+  // setTimeout(function() { svgWindow.print(); }, 1);
+}
